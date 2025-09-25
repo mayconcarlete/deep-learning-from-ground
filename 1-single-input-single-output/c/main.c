4fa135f81a49e5bf027eec74ad3a1825a0e5f92b
Maycon Carlete <maycon.carlete@gmail.com>
2025-09-24 22:20:14 -0300

    update single input/output c implementation

--- 1-single-input-single-output/c/main.c
+++ 1-single-input-single-output/c/main.c
@@ -1,9 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
+
 #include "simple_neural_network.h"
 
 int main(){
-    double output = single_in_single_out(10, 0.5);
-    printf("Output %.2f\n", output);
+    double temperature[] = { 10, 20, -30, 40 };
+    double weight = -2;
+
+    for(size_t i = 0; i < 4; i++){
+        double output = single_in_single_out(temperature[i], weight);
+        printf("Input [%ld] generated Output [%.2f]\n", i + 1, output);
+    }
+    
     return 0;
 }
--- 1-single-input-single-output/readme.md
+++ 1-single-input-single-output/readme.md
@@ -1,1 +1,3 @@
-### This folder contaings the C and C++ implementation for a Single Input / Single Output Neural Network
+## Single Input Single Output
+### This type of neural network mutiply the input x weight and generates an output.
+### This folder contaings the C and C++ implementation for a Single Input / Single Output Neural Network.
