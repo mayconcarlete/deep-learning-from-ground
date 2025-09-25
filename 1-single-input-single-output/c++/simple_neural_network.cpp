af383f7635cfa27a787097c1a266b416404ae461
--global <maycon.carlete@gmail.com>
2025-09-25 13:57:53 -0300

    refactor cpp code

--- 1-single-input-single-output/c++/simple_neural_network.cpp
+++ 1-single-input-single-output/c++/simple_neural_network.cpp
@@ -2,16 +2,19 @@
 #include <vector>
 #include <iostream>
 #include <iterator>
+#include <format>
+#include <string>
 
 SimpleInputSimpleOutput::SimpleInputSimpleOutput(const std::vector<double> inputs,const double weight):m_inputs(inputs),m_weight(weight){}
 
 SimpleInputSimpleOutput::~SimpleInputSimpleOutput(void){
-    std::cout << "Deleting Simple Input Simple Output class";
+    std::cout << "Deleting Simple Input Simple Output class" << "\n";
 }
 
 void SimpleInputSimpleOutput::perform(void){
     for(auto it = m_inputs.begin(); it != m_inputs.end(); ++it){
-        const auto index = std::distance(m_inputs.begin(), it);        
-        std::cout << "Input [" << index << "] value: ["<< *it << "] " << "generated Output ["<< *it * m_weight <<"]"<< "\n";
+        const auto index = std::distance(m_inputs.begin(), it);
+        const auto msg = std::format("Input [{}] Value: {} generated Output: {}", index, *it, *it * m_weight);              
+        std::cout << msg << "\n";
     }   
 }
