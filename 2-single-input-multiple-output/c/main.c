c9c34a60ac0865ff48a1cc654150b600cb930b10
Maycon Carlete <maycon.carlete@gmail.com>
2025-09-26 14:55:13 -0300

    single input multiple output

--- 2-single-input-multiple-output/c/main.c
+++ 2-single-input-multiple-output/c/main.c
@@ -6,9 +6,20 @@
 
 int main() {
     Predictions *result = malloc(sizeof(Predictions));
-    Weigths
-    double sad_input = 0.9;
+    
+    Weigths *weigths = malloc(sizeof(Weigths));
+    weigths->temperature_weigth = 2.0;
+    weigths->humidity_weigth = 3.0;
+    weigths->air_quality_weigth= 1.0;
 
-    single_input_multiple_output()
+    double sad_input = 2.0;
+    
+    single_input_multiple_output(sad_input, weigths, result);
+    printf("Results...\n");
+    printf("Temperature: %.2f Humidity: %.2f Air Quality: %.2f", result->temperature, result->humidity, result->air_quality);
+    printf("----------");
+
+    free(result);
+    free(weigths);
     return 0;
 }
